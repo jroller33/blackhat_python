2afef5c3da372ea842dd980af39b27e487b5bc79
John <john@kali.kali>
2023-06-25 21:47:36 -0500

    UDP_client

---
+++ project_1/UDP_client.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9997
+
+# create a socket obj
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send data
+client.sendto(b"AAABBBCCC", (target_host, target_port))
+
+# receive data
+data, addr = client.recvfrom(4096)
+
+print(data.decode)
+client.close()

322b6f751f4a67e59b172b62eac56a320484fd93
John <john@kali.kali>
2023-06-27 20:39:02 -0500

    next up: NetCat class

--- netcat/netcat.py
+++ netcat/netcat.py
@@ -18,6 +18,10 @@ def execute(cmd):
     return output.decode()
 
 
+class NetCat:
+    pass
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="John's Netcat",
@@ -26,6 +30,22 @@ if __name__ == '__main__':
         netcat.py -t 192.168.1.108 -p 5555 -l -c     # command shell
         netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt     # upload to file
         netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"    # execute command
-        echo 'ABC' | 
-    
+        echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135    # echo text to server port 135
+        netcat.py -t 192.168.1.108 -p 5555          # connect to server
     '''))
+
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type-int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nc = NetCat(args, buffer.encode())
+    nc.run()

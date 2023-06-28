d73a7c33780cbc329f4224fb3544f68f838a9e15
john_kali <john@kali.kali>
2023-06-27 21:31:33 -0500

    Netcat is working!

--- netcat/netcat.py
+++ netcat/netcat.py
@@ -125,7 +125,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type-int, default=5555, help='specified port')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
     parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
     parser.add_argument('-u', '--upload', help='upload file')
 

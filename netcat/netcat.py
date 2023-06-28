9fdde70e29dacdfd3947ef2b15d6b29703ef0ac7
John <john@kali.kali>
2023-06-27 20:18:42 -0500

    execute() is done

--- netcat/netcat.py
+++ netcat/netcat.py
@@ -6,3 +6,14 @@ import sys
 import textwrap
 import threading
 
+
+# receives a cmd, runs it, and returns the output as a string
+def execute(cmd):
+
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    
+    # 'check_output' method runs a cmd on the local OS and returns the output from the cmd
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()

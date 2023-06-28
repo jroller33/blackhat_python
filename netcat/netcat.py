f6f781aac7399b610fd446a118b6ac536770f93c
John <john@kali.kali>
2023-06-27 20:29:10 -0500

    working on main block of netcat

--- netcat/netcat.py
+++ netcat/netcat.py
@@ -9,7 +9,6 @@ import threading
 
 # receives a cmd, runs it, and returns the output as a string
 def execute(cmd):
-
     cmd = cmd.strip()
     if not cmd:
         return
@@ -17,3 +16,16 @@ def execute(cmd):
     # 'check_output' method runs a cmd on the local OS and returns the output from the cmd
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="John's Netcat",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Example:
+        netcat.py -t 192.168.1.108 -p 5555 -l -c     # command shell
+        netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt     # upload to file
+        netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"    # execute command
+        echo 'ABC' | 
+    
+    '''))

8844367ff16fee2a828730f06e161e2009c9e72a
jroller33 <jtroller33@gmail.com>
2023-07-01 12:56:19 -0500

    hexdump() and receive_from() are done

--- TCP_proxy/TCP_proxy.py
+++ TCP_proxy/TCP_proxy.py
@@ -2,27 +2,57 @@ import sys
 import socket
 import threading
 
+# this proxy has 4 parts:
+# - display communication between local and remote machines to the console (hexdump)
+# - receive data from an incoming socket from either the local or remote machine (receive_from)
+# - manage traffic direction between remote and local machines (proxy_handler)
+# - set up a listening socket and pass it to proxy_handler (server_loop)
+
+
+# HEXFILTER string contains ASCII characters, or a dot (.) if one doesn't exist
 HEX_FILTER = ''.join(
     [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)]
 )
 
+
+# This function lets you watch the data going through the proxy in real time
 def hexdump(src, length=16, show=True):
-    if isinstance(src, bytes):
+    if isinstance(src, bytes):  # make sure it's a string, decode the bytes if it's a byte string
         src = src.decode()
 
     results = list()
     for i in range(0, len(src), length):
-        word = str(src[i:i+length])
 
-        printable = word.translate(HEX_FILTER)
+        word = str(src[i:i+length]) # take a piece of the string and put it in 'word'
 
+        # use 'translate()' to substitute the str representation of each char for the corresponding char in the raw string (printable)
+        printable = word.translate(HEX_FILTER)  
+
+        # substitute the hex representation of the int value of every char in the raw string (hexa)
         hexa = ' '.join([f'{ord(c):02X}' for c in word])
         hexwidth = length * 3
+
+        # 'results' is a new list to hold the strings, contains the hex value of the index of teh first byte in the word, the hex value of the word, and its printable representation.
         results.append(f'{i:04x}  {hexa:<{hexwidth}}  {printable}')
 
     if show:
         for line in results:
             print(line)
-
     else:
         return results
+    
+
+def receive_from(connection): # for receiving local and remote data, pass in the socket object (connection) it will use
+    buffer = b""              # create an empty byte string that accumulates responses from the socket
+
+    connection.settimeout(10)      # 10 second timeout. For proxying traffic to other countries or over subpar networks, set the timeout longer
+    try:
+        while True:
+            data = connection.recv(4096)    # reads response data into the buffer, until there's no more data or reach timeout
+            if not data:
+                break
+            buffer += data
+
+    except Exception as e:
+        pass
+    return buffer   # return 'buffer' byte string to the caller (could be local or remote machine)

fab2a6ef7feaedbad354092fcedd4c418cd2702f
jroller33 <jtroller33@gmail.com>
2023-07-01 20:10:07 -0500

    ip_header_decode is mostly done

--- network_sniffers/sniffer_ip_header_decode.py
+++ network_sniffers/sniffer_ip_header_decode.py
@@ -4,6 +4,8 @@ import socket
 import struct
 import sys
 
+
+# maps the first 20 bytes of received buffer into a IP header
 class IP:
     def __init__(self, buff=None):
         header = struct.unpack('<BBHHHBBH4s4s', buff)
@@ -20,6 +22,7 @@ class IP:
         self.src = header[8]
         self.dst = header[9]
 
+        # make output readable that shows protocol and IP addresses
         self.src_address = ipaddress.ip_address(self.src)
         self.dst_address = ipaddress.ip_address(self.dst)
 
@@ -34,4 +37,44 @@ class IP:
             self.protocol = str(self.protocol_num)
 
     
-    
+def sniff(host):
+
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    try:
+        while True:
+            # read a packet
+            raw_buffer = sniffer.recvfrom(65535)[0]
+
+            # create an IP header from the first 20 bytes
+            ip_header = IP(raw_buffer[0:20])
+
+            # print the detected protocol and hosts
+            print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+
+    except KeyboardInterrupt:
+        # if on Windows, turn off promiscuous mode
+        if os.name == 'nt':
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = '192.168.1.203'
+    sniff(host)
+        

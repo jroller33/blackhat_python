c660717bc11bac883f441de17ea8e43dbb5aa3ca
jroller33 <jtroller33@gmail.com>
2023-07-02 10:49:37 -0500

    check wifi and kill conflicting processes

--- Denial_of_Service_WLAN/wifi_DoS.py
+++ Denial_of_Service_WLAN/wifi_DoS.py
@@ -55,3 +55,43 @@ for file_name in os.listdir():
         timestamp = datetime.now()
         
         shutil.move(file_name, directory + "/backup/" + str(timestamp) + "-" + file_name)
+
+
+# regex to find the wireless interfaces, if they're all wlan0 or higher
+wlan_pattern = re.compile("^wlan[0-9]+")
+
+
+# this uses the subprocess module to run system commands
+# iwconfig command looks for wireless interfaces
+check_wifi_result = wlan_pattern.findall(subprocess.run(["iwconfig"], capture_output=True).stdout.decode())
+
+
+# no wifi adapter connected
+if len(check_wifi_result) == 0:
+    print("Connect a wifi adapter and try again")
+    exit()
+    
+    
+# menu to select wifi
+print("The following WiFi interfaces are available:")
+for index, item in enumerate(check_wifi_result):
+    print(f"{index} - {item}")
+
+
+# check if the selected wifi is valid
+while True:
+    wifi_interface_choice = input("Please select the interface you want to use for the attack: ")
+    try:
+        if check_wifi_result[int(wifi_interface_choice)]:
+            break
+    except:
+        print("Enter the number of one of the choices")
+
+# user's selected wifi
+user_choice = check_wifi_result[int(wifi_interface_choice)]
+
+
+# have to kill any conflicting processes
+print("The wifi adapter is connected, now ending conflicting processes:")
+
+kill_conflict_processes = subprocess.run(["sudo", "airmon-ng", "check", "kill"])

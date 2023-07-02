bc39628f6f61096a164b765a08566d9767bbf05b
jroller33 <jtroller33@gmail.com>
2023-07-01 19:28:19 -0500

    basic sniffer

---
+++ network_sniffers/basic_sniffer.py
@@ -0,0 +1,3 @@
+# sniffer's main purpose is to discover hosts on a target network
+# it needs to work on Windows and Linux, even though the process of accessing raw sockets is different on Windows and Linux
+

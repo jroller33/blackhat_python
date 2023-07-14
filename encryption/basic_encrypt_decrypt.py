092d0fda35ab7dca35e7efdfbcac26580469946f
jroller33 <jtroller33@gmail.com>
2023-07-13 19:53:43 -0500

    getting started on encryption

---
+++ encryption/basic_encrypt_decrypt.py
@@ -0,0 +1,8 @@
+'''
+This script uses symmetric encryption built on the AES algorithm to encrypt or decrypt files, byte objects or string objects.
+
+
+'''
+
+from cryptography.fernet import Fernet
+

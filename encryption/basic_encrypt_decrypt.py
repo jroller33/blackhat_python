1d3e782f6b3a295a413f73e8d483b2d836b64c34
jroller33 <jtroller33@gmail.com>
2023-07-13 20:08:05 -0500

    write_key and load_key

--- encryption/basic_encrypt_decrypt.py
+++ encryption/basic_encrypt_decrypt.py
@@ -4,5 +4,35 @@ This script uses symmetric encryption built on the AES algorithm to encrypt or d
 
 '''
 
+'''
+very basic encryption example, using a string:
+
+write_key()
+key = load_key()
+message = "Dwight, you ignorant slut!".encode()
+f = Fernet(key)
+encrypted_message = f.encrypt(message)
+print(encrypted_message)
+
+decrypted_message = f.decrypt(encrypted_message)
+print(decrypted_message)
+
+'''
+
+# Fernet uses symmetric authenticated cryptography
 from cryptography.fernet import Fernet
+
+
+# generate a key and save it to a file
+def write_key():
+    key = Fernet.generate_key()                 # generates a fresh fernet key
+    with open("key.key", "wb") as key_file:     # don't lose the key, or you can't decrypt the files
+        key_file.write(key)
+
+
+# don't generate the key every time you encrypt something with the same key
+def load_key():
+    return open("key.key", "rb").read()     # loads the key from the directory 'key.key'
+
+
 

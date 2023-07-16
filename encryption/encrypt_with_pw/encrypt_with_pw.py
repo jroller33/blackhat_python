f083be2040fbdbd3da1d0804e532f11c67390128
jroller33 <jtroller33@gmail.com>
2023-07-15 20:40:18 -0500

    generate_key

--- encryption/encrypt_with_pw/encrypt_with_pw.py
+++ encryption/encrypt_with_pw/encrypt_with_pw.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
 import secrets      # use to make the salt
-import base64
+import base64       # use to encode the key
 import getpass      # securely handle password
 
 
@@ -11,6 +11,25 @@ import getpass      # securely handle password
 def generate_salt(size=16):
     return secrets.token_bytes(size)
 
+
 def derive_key(salt, password):
     kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
     return kdf.derive(password.encode())
+
+
+def load_salt():
+    return open("salt.salt", "rb").read()
+
+
+def generate_key(password, salt_size=16, load_existing_salt=False, save_salt=True):
+    if load_existing_salt:
+        salt = load_salt()
+
+    elif save_salt:
+        salt = generate_salt(salt_size)
+        with open("salt.salt", "wb") as salt_file:
+            salt_file.write(salt)
+
+    derived_key = derive_key(salt, password)
+
+    return base64.urlsafe_b64encode(derived_key)
